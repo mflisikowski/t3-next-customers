999bb7afcd90310d30afea74c5cce6f4c72069f6
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-09-20 20:56:00 +0200

    refactor(auth): remove commented-out code from server authentication
    - Removed unnecessary commented-out code related to user roles in the authentication module.

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -19,15 +19,8 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: DefaultSession["user"] & {
       id: string;
-      // ...other properties
-      // role: UserRole;
     };
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 /**

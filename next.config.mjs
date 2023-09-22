7e22a3dfe2baedffea652ba79437ea51529310a9
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-09-22 12:40:03 +0200

    feat(next.config): Add 'utfs.io' as an allowed image source
    - Expanded the `images` configuration in `next.config.mjs` to include `utfs.io` as an allowed remote image source. This enables fetching and optimizing images from `utfs.io` using Next.js' Image component.

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,12 @@ const config = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };

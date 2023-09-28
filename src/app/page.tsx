2ac810fd33b4d3b96bb2a9d66407243ff708573e
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-09-28 18:32:23 +0200

    refactor: simplify HomePage structure and integrate Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import LoginBtn from "@/components/login-btn";
+import Header from "@/components/header/header";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Next Customers
-        </h1>
-        <LoginBtn />
-      </div>
-    </main>
+    <>
+      <Header />
+
+      <main></main>
+    </>
   );
 }

d9d5e5af0f72b13408660e1acf1403a15ad3c003
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-09-28 18:37:39 +0200

    refactor(layout): simplify layout and introduce providers
    - Remove server-side session fetching from the layout.
    - Introduce a new `Providers` component to potentially wrap future context providers.
    - Adjust the layout structure for clarity and future extensibility.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,23 @@
-import Provider from "@/app/context/client-provider";
-import { authOptions } from "@/server/auth";
+export const metadata = {
+  title: "Next Customers",
+  description: "Next.js 13 T3 Stack with Prisma, TailwindCSS, and TypeScript",
+};
 import "@/styles/globals.css";
-import { getServerSession } from "next-auth/next";
+import { Providers } from "@/app/providers";
 import { Montserrat } from "next/font/google";
 import React from "react";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Next Customers",
-  description: "Next.js 13 T3 Stack with Prisma, TailwindCSS, and TypeScript",
-};
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}): Promise<JSX.Element> {
-  const session = (await getServerSession(authOptions))!;
-
+}): React.ReactNode {
   return (
     <html lang="en">
-      <body
-        className={`${montserrat.className} bg-primary min-w-screen min-h-screen`}
-      >
-        <Provider session={session}>{children}</Provider>
+      <body className={`${montserrat.className}`}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );

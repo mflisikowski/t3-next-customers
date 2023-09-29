91c7cd15120ba739fa332268f3c48cd531a372a3
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-09-29 22:05:55 +0200

    refactor(header): Integrate the Navigation Menu into the Header component
    - Integrated the `NavigationMenu` component into the `Header` component, allowing users to access navigation options from the header.
     - Adjusted the flexbox layout and gap spacing for the header items to accommodate the new `NavigationMenu`.
    - Moved the `AuthButton` component to a more appropriate position relative to the new `NavigationMenu`.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,7 @@ import AuthButton from "@/components/auth/auth-button";
 import AuthUserImage from "@/components/auth/auth-user-image";
 import Logo from "@/components/logo";
 import ThemeModeToggle from "@/components/theme-mode-toggle";
+import NavigationMenu from "../navigation/navigation-menu";
 
 export default function Header() {
   return (
@@ -11,9 +12,11 @@ export default function Header() {
           <div className="flex lg:flex-1">
             <Logo className="-m-1.5 p-1.5" />
           </div>
-          <div className="lg:flex lg:gap-x-12">
-            <AuthButton className="block px-4 py-2 text-center text-sm text-gray-700" />
+          <div className="lg:flex lg:gap-x-1">
+            <NavigationMenu />
+            <AuthButton />
           </div>
+
           <div className="lg:flex lg:flex-1 lg:justify-end">
             <div className="relative flex h-12 w-12 overflow-hidden rounded-full transition-shadow">
               <AuthUserImage className="absolute top-0 block h-12 w-12 object-cover" />

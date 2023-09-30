ccc5b16ee8c0fcb451f57bac6ce9c153fbca25fe
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-09-30 10:39:10 +0200

    feat(dashboard): add dashboard layout and simplify pages
    - Introduce new dashboard layout with side placeholders.
    - Add dashboard customers page.
    - Simplify the content of dashboard invoices page.

---
+++ src/app/(dashboard)/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardCustomersPage() {
+  return <main>Dashboard Customers Page</main>;
+}
--- src/app/(dashboard)/dashboard/invoices/page.tsx
+++ src/app/(dashboard)/dashboard/invoices/page.tsx
@@ -1,28 +1,3 @@
-import ThemeModeToggle from "@/components/theme-mode-toggle";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-
-export default function InvoicesPage() {
-  return (
-    <>
-      <section className="flex flex-wrap items-center gap-4 sm:flex-nowrap">
-        <h1 className="text-base font-semibold leading-7">Filter</h1>
-        <div className="order-last flex w-full items-center gap-x-4 sm:order-none sm:w-auto">
-          <Button variant="secondary">Last 7 days</Button>
-          <Button variant="ghost">Last 30 days</Button>
-          <Button variant="ghost">All-time</Button>
-        </div>
-
-        <div className="ml-auto flex items-center gap-x-4">
-          <Button variant="ghost" size="icon" className="rounded-full">
-            <Plus className="h-5 w-5" aria-hidden="true" />
-          </Button>
-
-          <ThemeModeToggle />
-        </div>
-      </section>
-
-      <main>Invoices</main>
-    </>
-  );
+export default function DashboardInvoicesPage() {
+  return <main>Dashboard Invoices Page</main>;
 }
